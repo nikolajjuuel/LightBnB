a1ce0751db98ea5a4fae2c6e64a63c8f6abfed59
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-10-15 02:46:49 +0000

    add reservations by useres

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -88,7 +88,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.property_id, properties.title, cost_per_night, reservations.start_date, reservations.end_date, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties on properties.id= property_id
+  JOIN property_reviews on reservations.id = reservation_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY reservations.property_id, properties.title, cost_per_night,reservations.start_date, reservations.end_date
+  ORDER BY start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
@@ -100,12 +116,12 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
-  pool
+ const getAllProperties = (options, limit) => {
+  return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
       console.log(result.rows);
-      properties = result.rows;
+      return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
